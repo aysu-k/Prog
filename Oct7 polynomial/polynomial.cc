3d4a3464113d9206066fc6365897c3eaf1d613cc
alme96 <37303359+alme96@users.noreply.github.com>
2019-10-12 17:26:46 +0200

    Update polynomial.cc

--- Oct7 polynomial/polynomial.cc
+++ Oct7 polynomial/polynomial.cc
@@ -1,61 +1,56 @@
 #include <iostream>
 #include "Polynomial.h"
+#include <vector>
+
 using namespace std;
 
 
 //default constructor, vector avec une taille initiale 100 et tous les elements nuls
-Polynomial::Polynomial() {
-		vector<double> coef(100);
-} 
-
-//constructor
-Polynomial(double coefficient, int dgr) 
-: degree(dgr), coef[dgr] = coefficient; {}
 
 
 int Polynomial::getDegree() {
 	
 		int d = 0;
 		for(int i(0); i < 100; ++i)
-			if (coef[i] != 0) d = i;
+			if (coeff[i] != 0) d = i;
 		return d;
 }
 
 
-Polynomial Polynomial::addition(Polynomial b) {
+Polynomial Polynomial::addition(Polynomial a, Polynomial b) {
 	
-	Polynomial a = *this;
 	Polynomial c;
 	
 	for (int i(0); i <= a.getDegree(); ++i) {
 		
-		c.coef[i] += a.coef[i];
+		c.coeff[i] += a.coeff[i];
 		
 	}
 	
 	for(int i(0); i <= b.getDegree(); ++i) {
-		c.coef[i] += b.coef[i];
+		c.coeff[i] += b.coeff[i];
 			
 	}
+
+	return c;
 }
 
-Polynomial Polynomial::multiplication(Polynomial b) {
+Polynomial Polynomial::multiplication(Polynomial a, Polynomial b) {
 	
-	Polynomial a = *this;
 	Polynomial c;
 	for (int i(0); i <= a.getDegree(); ++i) {
 		for (int j(0); j <= b.getDegree(); ++j) {
-			c.coef[i+j] += (a.coef[i] * b.coef[j]);
+			c.coeff[i+j] += (a.coeff[i] * b.coeff[j]);
 		}
 	}
-	c.deg = c.getDegree(); // I think that we still have to group the coefficients afterwards.
+	c.dgr = c.getDegree(); // I think that we still have to group the coefficients afterwards.
 	return c;
 }
 
 void Polynomial::print(){
 	
 	for(int i(99); i>=0; --i) {
-		if(coef[i] != 0) {	cout << coef[i] << "*" << "x^" << i << " " << endl; }
+		if(coeff[i] != 0) {	cout << coeff[i] << "*" << "x^" << i << " " << endl; }
 	}
 }
 
