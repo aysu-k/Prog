b436c736ac714e22cef334e7d3be1b747888d903
alme96 <37303359+alme96@users.noreply.github.com>
2019-10-12 17:25:21 +0200

    Update main.cc

--- Oct7 polynomial/main.cc
+++ Oct7 polynomial/main.cc
@@ -1,11 +1,15 @@
 
+#include <iostream>
+#include "Polynomial.h"
+#include "Polynomial.cpp"
+using namespace std;
+
 int main() {
-	
-	Polynomial a(7,1); // Shouldn't the first input be a coefficient array?
-	Polynomial b(3,5);
-	
-	
-	
-	
+
+	Polynomial a({0, -2, 7, -8},3); // Shouldn't the first input be a coefficient array?
+	Polynomial b({10, 0, -3, 4},4);
+
+	multiplication(a, b);
+
 	return 0;
-	}
+}
