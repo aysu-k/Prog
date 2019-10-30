fbeae9e636b299c7b5d8613a35000b1e2be527da
alme96 <37303359+alme96@users.noreply.github.com>
2019-10-30 12:04:42 +0100

    Add files via upload

---
+++ Project/Domotique/Sources/main.cpp
@@ -0,0 +1,17 @@
+#include <list>
+#include <string>
+#include <stdio.h>
+#include <list>
+#include <iostream>
+#include "tinyxml2/tinyxml2.h"
+#include <Test_XML_AVx>
+
+using namespace std;
+
+int main()
+{
+	XMLDocument* doc = new XMLDocument ;
+	("C:\Users\menichea\OneDrive - ETHZ\ETH\Master\1. Semester\Object-oriented programming and design\Projet\domotique.xml");
+
+}
+
